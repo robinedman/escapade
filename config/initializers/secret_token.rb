28ad92d9d516899e43b27f2abba3ded2da3dd694
Robin Edman <robin.edman@gmail.com>
2014-03-17 09:37:46 +0000

    Empty Rails repo with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Escapade::Application.config.secret_key_base = '8688d171b29281a49d8e1424b60d5a1a9a6041b5b92d688ff0f24b116cd4352e1ca4f5943ece4e36e1ca5e1f7fb721857091ef3570d26baff8dda5fa423eda1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Escapade::Application.config.session_store :cookie_store, key: '_escapade_session'
